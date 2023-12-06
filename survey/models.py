66d1c8deb9d73e1ad4615cae45763c73100b355e
Oia20 <jdement97@gmail.com>
2023-12-05 21:30:17 -0600

    Lots of updates to html and css. Connected to django db, next is to display the data and structure it better.

--- survey/models.py
+++ survey/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+COLOR_CHOICES = (
+    ('less than one.','less than one.'),
+    ('one', 'one'),
+    ('three','three'),
+    ('four','four'),
+    ('five','five'),
+    ('six or more.', 'six or more.')
+)
+
+seasons = (
+    ('summer', 'SUMMER'),
+    ('spring', 'SPRING'),
+    ('winter', 'WINTER'),
+    ('fall', 'FALL')
+
+)
 # Create your models here.
+class surverySubmit(models.Model):
+    fname = models.CharField(max_length=30, null=False, blank=False)
+    strokes= models.IntegerField(null=False, blank=False)
+    years = models.CharField(max_length=30, choices=COLOR_CHOICES, default='one')
+    season = models.CharField(max_length=30, choices=seasons, default='summer')
+    areas = models.BooleanField()
--- survey/views.py
+++ survey/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-
+from .models import surverySubmit
 # Create your views here.
+
 def golfsurv(request): 
-      
-    # render function takes argument  - request 
-    # and return HTML as response 
+    if request.method == 'POST':
+        fname = request.POST['fname']
+        strokes = request.POST['strokes']
+        years = request.POST['years']
+        seasons = request.POST['seasons']
+        areas = request.POST['areas']
+
+        new_surv = surverySubmit(fname=fname, strokes=strokes, years=years, season=seasons, areas=areas)
+        new_surv.save()
     return render(request, "golf.html")
